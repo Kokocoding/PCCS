763675045d8ff3b64aacfec062e1b030963286f5
stytle704726 <stytle704726@gmail.com>
2023-08-28 11:43:41 +0800

    no message

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -56,7 +56,7 @@ namespace PCCS {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PCCS;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PCCS;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
